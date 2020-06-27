fc0df31daa61fca752226a3b1a1da7df9d9b2a65
Xavier <aj449446@gmail.com>
2020-06-27 19:28:31 +0530

    Removed unnecessary files and directories

--- temp.py
+++
@@ -1,9 +0,0 @@
-import sounddevice as sd
-from scipy.io.wavfile import write
-
-fs = 4410  # Sample rate
-seconds = 4  # Duration of recording
-
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-sd.wait()  # Wait until recording is finished
-write('output.wav', fs, myrecording) 
